638f08d9b3081999dd924b14527656cf0388cc8e
kazuki827 <xm.kazuki827@gmail.com>
2022-05-29 21:40:42 +0900

    test of CRUD and SSG

--- __test__/HasuraSSG.test.tsx
+++ __test__/HasuraSSG.test.tsx
@@ -1,4 +1,5 @@
 import { cleanup, findByText, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
 import { getPage, initTestHelpers } from 'next-page-tester'
 import { setupServer } from 'msw/node'
@@ -15,16 +16,16 @@ afterEach(() => {
 })
 afterAll(() => server.close())
 
-describe('Hasura Fetch Test Cases', () => {
-  it('Should render the kist if user by useQuery', async () => {
+describe('SSG Test Cases', () => {
+  it('Should render the list of users pre-fetched by getStaticProps', async () => {
     const { page } = await getPage({
-      route: '/hasura-main',
+      route: '/hasura-ssg',
     })
     render(page)
 
-    expect(await screen.findByText('Hasura main page')).toBeInTheDocument()
+    expect(await screen.findByText('SSG+ISR')).toBeInTheDocument()
 
-    expect(await screen.findByText('Test user A')).toBeInTheDocument()
+    expect(screen.getByText('Test user A')).toBeInTheDocument()
     expect(screen.getByText('Test user B')).toBeInTheDocument()
     expect(screen.getByText('Test user C')).toBeInTheDocument()
   })
